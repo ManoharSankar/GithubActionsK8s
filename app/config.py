6df920bfb0487e3cd792b567d564b22d457e019b
ManoharSankar <manoharsankar93@gmail.com>
2026-01-19 13:01:06 +0530

    added the files

--- app/config.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseSettings
-
-class Settings(BaseSettings):
-    SERVICE_NAME: str = "python-microservice"
-
-settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,17 +1,7 @@
 from fastapi import FastAPI
-from app.config import settings
 
-app = FastAPI(title="Production Python Microservice")
-
-@app.get("/")
-def root():
-    return {"message": "Hello from Kubernetes microservice"}
+app = FastAPI()
 
 @app.get("/health")
 def health():
     return {"status": "UP"}
-
-@app.get("/config")
-def config():
-    return {"service_name": settings.SERVICE_NAME}
-
